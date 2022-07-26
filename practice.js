bd9bc7cc2c28dd23a854e06d71d10587b5f6bdf4
Maria Redman <mariaredman@Marias-MacBook-Air-2.local>
2022-07-25 23:52:45 -0400

    At home coding Practice

---
+++ practice.js
@@ -0,0 +1,49 @@
+
+// let hours = 2
+// function howManyseconds(hours){
+
+// let seconds = (hours * 60) * 60;
+// return seconds 
+// }
+// console.log(howManySeconds(2, 10, 24))
+
+// function sum(num1, num2){
+// return num1 + num2 
+
+// }
+// console.log(sum(2, 3))
+
+// let person = {
+// name:"Maria",
+// age:50,
+// }
+// console.log(person.name , "is not as lost")
+
+// const countDown = () => {
+// for(let i=10; i>0; i--){
+// console.log(i)
+// }
+ 
+// }
+//   countDown()
+
+//Convert minutes into seconds
+
+// function convert(minutes) {
+// 	return  minutes * 60;
+// }
+
+
+
+
+
+// const countdown = (number) => {
+//     let arrayCount = [];
+//     for(let i = 0; i <= number; i++) {
+//         arrayCount.push(number - i)
+//     }
+//   return arrayCount
+//   }
+  
+  
+//   console.log(countdown(10));
